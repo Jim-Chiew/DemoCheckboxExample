fefdeea8b0d5a64731c2eaa60592f7297f9563c1
Jim Chiew <20004713@myrp.edu.sg>
2021-05-10 13:15:01 +0800

    Update on the WS. 1:14pm

--- app/src/main/java/sg/edu/rp/c346/id20004713/democheckboxexample/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20004713/democheckboxexample/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id20004713.democheckboxexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -26,16 +27,26 @@ public class MainActivity extends AppCompatActivity {
         btnCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (cbDiscount.isChecked()){
-                    tvOutput.setText("Discount is given");
-                    cbDiscount.setChecked(false);
-                } else {
-                    tvOutput.setText("Discount is not given");
-                    cbDiscount.setChecked(true);
+                Log.i("MyActivity", "Inside onClick()");
+                Toast.makeText(MainActivity.this, "Button Click", Toast.LENGTH_SHORT).show();
+
+                if(cbDiscount.isChecked()) {
+                    double pay = calcPay(100, 20);
+                    tvOutput.setText("The discount is given. You need to pay " + pay);
+                }
+                else {
+                    double pay = calcPay(100, 0);
+                    tvOutput.setText("The discount is not given. You need to pay " + pay);
                 }
 
-                Toast.makeText(MainActivity.this, "Button Click", Toast.LENGTH_SHORT).show();
+
+
             }
         });
+    }
+
+    private double calcPay(double price, double discount){
+        double pay = price * (1 - discount / 100);
+        return pay;
     }
 }
